d4e672ce7363e6797c866954c41d42b66aa21fd7
Andrey Konovalov <adech.fo@gmail.com>
2014-09-22 14:49:32 +0400

    Update popup menu && add vowels

--- popup.js
+++ popup.js
@@ -1,13 +1,21 @@
 function click(e) {
-  if (e.target.id == "enable") {
+  if (e.target.innerHTML == "Enable") {
     chrome.storage.local.set({"enabled": "true"});
-  } else if (e.target.id == "disable") {
+  } else if (e.target.innerHTML == "Disable") {
     chrome.storage.local.set({"enabled": "false"});
   } 
   window.close();
 }
 
-document.addEventListener('DOMContentLoaded', function() {
+$(document).ready(function() {
+  chrome.storage.local.get("enabled", function (result) {
+    if (result.enabled == "true") {
+      document.getElementById("switch").innerHTML = "Disable";
+    } else {
+      document.getElementById("switch").innerHTML = "Enable";
+    }
+  });
+
   var divs = document.querySelectorAll('div');
   for (var i = 0; i < divs.length; i++) {
     divs[i].addEventListener('click', click);
