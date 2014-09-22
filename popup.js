0c69cbfc00ee7c9e3b10b8078a92e2e2e2bd6e59
Andrey Konovalov <adech.fo@gmail.com>
2014-09-22 18:53:21 +0400

    Add Options item to popup

--- popup.js
+++ popup.js
@@ -1,37 +1,24 @@
-function click(e) {
-  if (e.target.innerHTML == "Enable") {
-    chrome.storage.local.set({"enabled": "true"});
-    //openOptions();
-  } else if (e.target.innerHTML == "Disable") {
-    chrome.storage.local.set({"enabled": "false"});
-    //openOptions();
-  }
+$("#enable").click(function() {
+  chrome.storage.local.set({"enabled": true});
   window.close();
-}
+});
+
+$("#disable").click(function() {
+  chrome.storage.local.set({"enabled": false});
+  window.close();
+});
+
+$("#options").click(function() {
+  chrome.tabs.create({url: "options.html"});
+  window.close();
+});
 
 $(document).ready(function() {
   chrome.storage.local.get({"enabled": true}, function (result) {
     if (result.enabled) {
-      document.getElementById("switch").innerHTML = "Disable";
+      $("#enable").hide();
     } else {
-      document.getElementById("switch").innerHTML = "Enable";
+      $("#disable").hide();
     }
   });
-
-  var divs = document.querySelectorAll('div');
-  for (var i = 0; i < divs.length; i++) {
-    divs[i].addEventListener('click', click);
-  }
 });
-
-/*function openOptions() {
-  var options_url = chrome.extension.getURL('options.html');
-  chrome.tabs.query({
-      url: options_url,
-  }, function(results) {
-      if (results.length)
-          chrome.tabs.update(results[0].id, {active:true});
-      else
-          chrome.tabs.create({url:options_url});
-  })
-}*/
