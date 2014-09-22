42d907a14365a89adfc88cb59e1ef5f94f6ecab8
Andrey Konovalov <adech.fo@gmail.com>
2014-09-22 19:55:11 +0400

    Add site local disabling / enabling

--- on_document_end.js
+++ on_document_end.js
@@ -1,5 +1,10 @@
-chrome.storage.local.get({"enabled": true, "frequency": 0.1}, function(result) {
-  if (result.enabled) {
-    katakanizeHtml(result.frequency);
+chrome.storage.local.get({"enabled": true, "frequency": 0.1, "ignore": {}}, function(result) {
+  chrome.storage.local.set({"last_site": window.location.host});
+  if (!result.enabled) {
+    return;
   }
+  if (window.location.host in result.ignore) {
+    return;
+  }
+  katakanizeHtml(result.frequency);
 });
--- popup.js
+++ popup.js
@@ -8,17 +8,44 @@ $("#disable").click(function() {
   window.close();
 });
 
+$("#enable-site").click(function() {
+  chrome.storage.local.get({"ignore": {}, "last_site": ""}, function (result) {
+    if (result.last_site != "") {
+      var ignore = result.ignore;
+      delete ignore[result.last_site];
+      chrome.storage.local.set({"ignore": ignore});
+    }
+    window.close();
+  });
+});
+
+$("#disable-site").click(function() {
+  chrome.storage.local.get({"ignore": {}, "last_site": ""}, function (result) {
+    if (result.last_site != "") {
+      var ignore = result.ignore;
+      ignore[result.last_site] = true;
+      chrome.storage.local.set({"ignore": ignore});
+    }
+    window.close();
+  });
+});
+
 $("#options").click(function() {
   chrome.tabs.create({url: "options.html"});
   window.close();
 });
 
 $(document).ready(function() {
-  chrome.storage.local.get({"enabled": true}, function (result) {
+  chrome.storage.local.get({"enabled": true, "ignore": {}, "last_site": ""}, function (result) {
     if (result.enabled) {
       $("#enable").hide();
     } else {
       $("#disable").hide();
+    }
+    if (result.last_site in result.ignore) {
+      $("#disable-site").hide();
+    } else {
+      $("#enable-site").hide();
     }
   });
 });
