f968959040f45c333fcc7cc934065a0c70bd1590
Andrey Konovalov <adech.fo@gmail.com>
2014-09-22 19:14:49 +0400

    Workaround slider blinking

--- options.js
+++ options.js
@@ -1,15 +1,21 @@
-// FIXME(xairy): slider and text blink on page refresh.
-
 var original = document.getElementById('sample-text').innerHTML;
 
-var slider = $('#frequency').slider({
-  tooltip: 'always'
-}).on('slide', function(slider) {
-    var frequency = parseInt(slider.value) / 100.0;
-    document.getElementById("sample-text").innerHTML = katakanizeText(original, frequency);
-    chrome.storage.local.set({"frequency": frequency});
-});
+function slideCallback(slider) {
+  if (typeof slider.value == "undefined") {
+    return;
+  }
+  var frequency = parseInt(slider.value) / 100.0;
+  document.getElementById('sample-text').innerHTML = katakanizeText(original, frequency);
+  chrome.storage.local.set({'frequency': frequency});
+}
 
-chrome.storage.local.get({"frequency": 0.1}, function(result) {
-  slider.slider('setValue', result.frequency * 100, true);
+$(document).ready(function() {
+  var slider = $('#frequency').slider({
+    tooltip: 'always'
+  }).on('slide', slideCallback).on('slideStop', slideCallback);
+
+  chrome.storage.local.get({'frequency': 0.1}, function(result) {
+    slider.slider('setValue', result.frequency * 100, true);
+    $("#frequency-div").show("fast");
+  });
 });
